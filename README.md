2c4826152dd953cf0dd0aadef83f3bf00dcbe321
Otto Takacs <otto.takacs@sword-group.com>
2017-02-14 14:28:23 +0100

    REF: change javadoc refs in Resolver ADD: initial scripts for uploading to maven central repository

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Plan:
 3.3 copy properties obj from field/attr to field/attr
 4. Make it working with non public beans.
 5. [Done] Add null attribute access. Example: `attr1?.attr2`
-6. Add collection accessors. Example. Taken `attr1` as a `List` expression `attr1.attr2` gets all the attr2 values in collection. 
+6. Add collection accessors. Example. Taken `attr1` as a `List` expression `attr1*.attr2` gets all the attr2 values in collection. See Groovy spread operator. (http://docs.groovy-lang.org/latest/html/documentation/#_spread_operator)
 
 Nullable attribute access has a limitation. 
 When the attribute is accesse like `getIndexedProp(int index)` then the underlying method must take care of nullability.
--- src/main/java/eu/qualityontime/commons/expression/Resolver.java
+++ src/main/java/eu/qualityontime/commons/expression/Resolver.java
@@ -58,117 +58,106 @@ package eu.qualityontime.commons.expression;
  * BeanUtils/PropertyUtils uses the <code>resolver</code>. The following are the
  * main methods that use it:
  * <ul>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean}
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean}
  * <ul>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean#getIndexedProperty(Object, String)}</li>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean#getMappedProperty(Object, String)}</li>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean#getNestedProperty(Object, String)}</li>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean#getPropertyDescriptor(Object, String)}</li>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean#getSimpleProperty(Object, String)}</li>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean#setIndexedProperty(Object, String, Object)}</li>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean#setMappedProperty(Object, String, Object)}</li>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean#setNestedProperty(Object, String, Object)}</li>
- * <li>{@link org.apache.commons.beanutils.PropertyUtilsBean#setSimpleProperty(Object, String, Object)}</li>
- * </ul>
- * </li>
- * <li>{@link org.apache.commons.beanutils.BeanUtilsBean}
- * <ul>
- * <li>{@link org.apache.commons.beanutils.BeanUtilsBean#copyProperty(Object, String, Object)}</li>
- * <li>{@link org.apache.commons.beanutils.BeanUtilsBean#setProperty(Object, String, Object)}</li>
- * </ul>
- * </li>
- * <li>{@link org.apache.commons.beanutils.locale.LocaleBeanUtilsBean}
- * <ul>
- * <li>{@link org.apache.commons.beanutils.locale.LocaleBeanUtilsBean#setProperty(Object, String, Object, String)}</li>
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean#getIndexedProperty(Object, String)}</li>
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean#getMappedProperty(Object, String)}</li>
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean#getNestedProperty(Object, String)}</li>
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean#getPropertyDescriptor(Object, String)}</li>
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean#getSimpleProperty(Object, String)}</li>
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean#setIndexedProperty(Object, String, Object)}</li>
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean#setMappedProperty(Object, String, Object)}</li>
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean#setNestedProperty(Object, String, Object)}</li>
+ * <li>{@link eu.qualityontime.commons.QPropertyUtilsBean#setSimpleProperty(Object, String, Object)}</li>
  * </ul>
  * </li>
  * </ul>
  *
  * @version $Id$
- * @see org.apache.commons.beanutils.PropertyUtilsBean#setResolver(Resolver)
+ * @see eu.qualityontime.commons.QPropertyUtilsBean#setResolver(Resolver)
  * @since 1.8.0
  */
 public interface Resolver {
 
-	/**
-	 * Extract the index value from the property expression or -1.
-	 *
-	 * @param expression
-	 *            The property expression
-	 * @return The index value or -1 if the property is not indexed
-	 * @throws IllegalArgumentException
-	 *             If the indexed property is illegally formed or has an invalid
-	 *             (non-numeric) value
-	 */
-	int getIndex(String expression);
+  /**
+   * Extract the index value from the property expression or -1.
+   *
+   * @param expression
+   *            The property expression
+   * @return The index value or -1 if the property is not indexed
+   * @throws IllegalArgumentException
+   *             If the indexed property is illegally formed or has an invalid
+   *             (non-numeric) value
+   */
+  int getIndex(String expression);
 
-	/**
-	 * Extract the map key from the property expression or <code>null</code>.
-	 *
-	 * @param expression
-	 *            The property expression
-	 * @return The index value
-	 * @throws IllegalArgumentException
-	 *             If the mapped property is illegally formed
-	 */
-	String getKey(String expression);
+  /**
+   * Extract the map key from the property expression or <code>null</code>.
+   *
+   * @param expression
+   *            The property expression
+   * @return The index value
+   * @throws IllegalArgumentException
+   *             If the mapped property is illegally formed
+   */
+  String getKey(String expression);
 
-	/**
-	 * Return the property name from the property expression.
-	 *
-	 * @param expression
-	 *            The property expression
-	 * @return The property name
-	 */
-	String getProperty(String expression);
+  /**
+   * Return the property name from the property expression.
+   *
+   * @param expression
+   *            The property expression
+   * @return The property name
+   */
+  String getProperty(String expression);
 
-	/**
-	 * Indicates whether or not the expression contains nested property
-	 * expressions or not.
-	 *
-	 * @param expression
-	 *            The property expression
-	 * @return The next property expression
-	 */
-	boolean hasNested(String expression);
+  /**
+   * Indicates whether or not the expression contains nested property
+   * expressions or not.
+   *
+   * @param expression
+   *            The property expression
+   * @return The next property expression
+   */
+  boolean hasNested(String expression);
 
-	/**
-	 * Indicate whether the expression is for an indexed property or not.
-	 *
-	 * @param expression
-	 *            The property expression
-	 * @return <code>true</code> if the expresion is indexed, otherwise
-	 *         <code>false</code>
-	 */
-	boolean isIndexed(String expression);
+  /**
+   * Indicate whether the expression is for an indexed property or not.
+   *
+   * @param expression
+   *            The property expression
+   * @return <code>true</code> if the expresion is indexed, otherwise
+   *         <code>false</code>
+   */
+  boolean isIndexed(String expression);
 
-	/**
-	 * Indicate whether the expression is for a mapped property or not.
-	 *
-	 * @param expression
-	 *            The property expression
-	 * @return <code>true</code> if the expresion is mapped, otherwise
-	 *         <code>false</code>
-	 */
-	boolean isMapped(String expression);
+  /**
+   * Indicate whether the expression is for a mapped property or not.
+   *
+   * @param expression
+   *            The property expression
+   * @return <code>true</code> if the expresion is mapped, otherwise
+   *         <code>false</code>
+   */
+  boolean isMapped(String expression);
 
-	/**
-	 * Extract the next property expression from the current expression.
-	 *
-	 * @param expression
-	 *            The property expression
-	 * @return The next property expression
-	 */
-	String next(String expression);
+  /**
+   * Extract the next property expression from the current expression.
+   *
+   * @param expression
+   *            The property expression
+   * @return The next property expression
+   */
+  String next(String expression);
 
-	/**
-	 * Remove the last property expresson from the current expression.
-	 *
-	 * @param expression
-	 *            The property expression
-	 * @return The new expression value, with first property expression removed
-	 *         - null if there are no more expressions
-	 */
-	String remove(String expression);
+  /**
+   * Remove the last property expresson from the current expression.
+   *
+   * @param expression
+   *            The property expression
+   * @return The new expression value, with first property expression removed
+   *         - null if there are no more expressions
+   */
+  String remove(String expression);
 
 }
